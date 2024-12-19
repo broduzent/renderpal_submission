f23aef58d815df008bbbff034eda6dd9349d0d9e
Bene <benedikt.roesch@googlemail.com>
2024-12-19 09:38:45 +0100

    support all color profiles

--- renderpal_submission/autocomp/imgconvert.py
+++ renderpal_submission/autocomp/imgconvert.py
@@ -7,8 +7,33 @@ print(sys.argv[1:])
 in_pattern, out_file, in_frame, out_frame, colorspace = sys.argv[1:]
 
 cspace_dict = {
+    "default": 0,
     "linear": 1,
     "srgb": 2,
+    "rec709": 3,
+    "cineon": 4,
+    "gamma1.8": 5,
+    "gamma2.2": 6,
+    "gamma2.4": 7,
+    "gamma2.6": 8,
+    "panalog": 9,
+    "redlog": 10,
+    "viperlog": 11,
+    "alexav3logc": 12,
+    "plogin": 13,
+    "slog": 14,
+    "slog1": 15,
+    "slog2": 16,
+    "slog3": 17,
+    "clog": 18,
+    "log3g10": 19,
+    "log3g12": 20,
+    "hybridloggamma": 21,
+    "protune": 22,
+    "bt1886": 23,
+    "st2084": 24,
+    "blackmagicfilmgeneration5": 25,
+    "arrilogc4": 26,
 }
 
 in_frame = int(in_frame[-4:])
@@ -23,7 +48,7 @@ print(f"Writing frames to {out_file}")
 nuke.root()['fps'].setValue(25)
 r = nuke.nodes.Read()
 w = nuke.nodes.Write()
-w.setInput( 0, r )
+w.setInput(0, r)
 w.knob("file_type").setValue("mov")
 print("Node setup done")
 r.knob("file").setValue(in_pattern)
